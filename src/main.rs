5c6fd5795f7c5019c630622369a67f157c82cd07
Gintautas Kazlauskas <gintautaskazlauskas@protonmail.com>
2020-03-02 17:53:40 +0200

    added .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 use std::env;
-use serde_json::{Value};
+use serde_json::Value;
 
 fn main() {
 
-  // let mut results: u8 = 1;
+  // let results: usize = 1;
 
-  let word: Vec<String> = env::args().collect();
-  // println!("{}", word[1].as_str());
+  let args: Vec<String> = env::args().collect();
+  let word = &args[1];
+  let results: usize = args[2].parse().unwrap();
+
+  println!("Finding definition for {}", word);
+
+  let json_response = get_json(word);
+
+  for i in 0..results {
+    let j_word = &json_response["data"][i]["japanese"][0]["word"];
+    let j_reading = &json_response["data"][i]["japanese"][0]["reading"];
+    let e_word = &json_response["data"][i]["senses"][0]["english_definitions"][0];
+
+    println!("{} ({}) - {}", j_word.as_str().unwrap(), j_reading.as_str().unwrap(), e_word.as_str().unwrap());
+
+  }
+
+
+
+
+}
+
+fn get_json(word: &str) -> Value {
   let jisho = ureq::get("https://jisho.org/api/v1/search/words")
-    .query("keyword", word[1].as_str())
+    .query("keyword", word)
     .call();
 
-  let json_string = jisho.into_string().unwrap().as_str();
-  let json_response: Value = serde_json::from_str(json_string);
-    // [location] .as_str().unwrap().replace("\"", "");
-
-  // let j_word = json_response["data"][0]["japanese"][0]["word"].as_str().unwrap().replace("\"", "");
-  // let e_word = json_response["data"][0]["senses"][0]["english_definitions"][0].as_str().unwrap().replace("\"", "");
-
-
-  // println!("{}", json_response["data"][0]["japanese"][0]["word"]);
-  // println!("{} ", e_word);
-
+  return jisho.into_json().unwrap();
 }
